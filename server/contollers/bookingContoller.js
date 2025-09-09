32c6284bb3dd7fc1a7df9c3677abe5b240f0f60a
Vansh <narulavansh430@gmail.com>
2025-09-09 11:48:09 +0530

    function added

--- server/contollers/bookingContoller.js
+++ server/contollers/bookingContoller.js
@@ -1,3 +1,4 @@
+import { inngest } from "../inngest/index.js";
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js"
 import stripe from 'stripe'
@@ -80,6 +81,13 @@ export const createBooking =async(req,res)=>{
         booking.paymentLink=session.url
         await booking.save()
 
+        //Run inngest scheduler function to check payment status after 10 minutes
+        await inngest.send({
+            name:'app/checkpayment',
+            data:{
+                bookingId: booking._id.toString()
+            }
+        })
 
         res.json({success:true,url:session.url})
 
--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
 import User from "../models/User.js";
+import Booking from "../models/Booking.js";
+import Show from "../models/Show.js";
 
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
@@ -46,6 +48,32 @@ const syncUserUpdation=inngest.createFunction(
     }
 )
 
+//inngest function to cancel booking and release seats of show after 10 minutes
+const releaseSeatsAndDeleteBooking=inngest.createFunction(
+    {id:'release-seats-delete-booking'},
+    {event:'app/checkpayment'},
+    async({event,step})=>{
+      const tenMinutesLater=new Date(Date.now()+10*60*1000)
+      await step.sleepUntil('wait-for-10-minutes',tenMinutesLater)
+
+      await step.sleepUntil('check-payment-status',async()=>{
+        const bookingId =event.data.bookingId;
+        const booking =await Booking.findById(bookingId)
+
+        //if payment is not made,release seats and delete booking
+        if(!booking.isPaid){
+            const show = await Show.findById(booking.show)
+            booking.bookedSeats.forEach(()=>{
+                delete show.occupiedSeats[seat]
+            })
+            show.markModified('occupiedSeats')
+            await show.save()
+            await Booking.findByIdAndDelete(booking._id)
+        }
+      })
+    }
+)
 
 
-export const functions=[syncUserCreation,syncUserDeletion,syncUserUpdation]
+
+export const functions=[syncUserCreation,syncUserDeletion,syncUserUpdation,releaseSeatsAndDeleteBooking]
